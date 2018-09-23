fdc3ff34d9754166ac34de784e13abe1f7fccde7
Burnsedia <bburnsed1995@gmail.com>
2018-09-23 13:57:48 -0400

    basic layout

---
+++ Charitorclass.cs
@@ -0,0 +1,18 @@
+namespace the_voyige
+{
+    public class Charitorclass
+    {
+        public int hp {set; get;}
+        public int MaxHp {set;get;}
+        public int dp {set;get;}
+
+
+        // public void takeDammage()
+        // {
+        //     if(hp <= 0)
+        //     {
+        //         //kill player and/or NPC
+        //     }
+        // }
+    }
+}
